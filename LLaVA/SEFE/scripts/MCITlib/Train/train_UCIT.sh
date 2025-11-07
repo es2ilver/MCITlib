26043204b6b06e66c82da790ab9a50400d7c575c
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-07 22:55:09 +0900

    modified hide-llava config

--- LLaVA/SEFE/scripts/MCITlib/Train/train_UCIT.sh
+++ LLaVA/SEFE/scripts/MCITlib/Train/train_UCIT.sh
@@ -25,9 +25,9 @@ bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 3
 
 pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
-    $HARD_PATH/configs/modal_configs/llava.json \
-    $HARD_PATH/configs/data_configs/UCIT/IconQA.json \
-    $HARD_PATH/configs/train_configs/SEFE/LLaVA/UCIT/train/task4.json
+    /home/data/vgilab/jeongeun/MCITlib/configs/modal_configs/llava.json \
+    /home/data/vgilab/jeongeun/MCITlib/configs/data_configs/UCIT/IconQA.json \
+    /home/data/vgilab/jeongeun/MCITlib/configs/train_configs/SEFE/LLaVA/UCIT/train/task4.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 4
 
 pip install -e .
