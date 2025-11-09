245bb80c92b233721e817b9c4e3eb29d0ad2dcbb
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-09 22:09:36 +0900

    modified disco env

--- LLaVA/DISCO/scripts/MCITlib/Train/train_UCIT.sh
+++ LLaVA/DISCO/scripts/MCITlib/Train/train_UCIT.sh
@@ -3,41 +3,38 @@
 HARD_PATH=/home/data/vgilab/jeongeun/MCITlib
 
 pip install -e .
+pip install -e .[train]
+
 bash scripts/MCITlib/Train/Task1.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/ImageNet-R.json \
     $HARD_PATH/configs/train_configs/DISCO/LLaVA/UCIT/train/task1.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 1
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/ArxivQA.json \
     $HARD_PATH/configs/train_configs/DISCO/LLaVA/UCIT/train/task2.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 2
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/VizWiz.json \
     $HARD_PATH/configs/train_configs/DISCO/LLaVA/UCIT/train/task3.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 3
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/IconQA.json \
     $HARD_PATH/configs/train_configs/DISCO/LLaVA/UCIT/train/task4.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 4
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/CLEVR-Math.json \
     $HARD_PATH/configs/train_configs/DISCO/LLaVA/UCIT/train/task5.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 5
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/Flickr30k.json \
