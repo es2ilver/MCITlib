60a58b76af5724d5557c16409796322d6fb40e05
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-10 14:03:05 +0900

    add exist_ok option.

--- LLaVA/DISCO/llava/model/language_model/llava_llama.py
+++ LLaVA/DISCO/llava/model/language_model/llava_llama.py
@@ -150,5 +150,5 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             _inputs['images'] = images
         return _inputs
 
-AutoConfig.register("llava", LlavaConfig)
+AutoConfig.register("llava", LlavaConfig, exist_ok=True)
 AutoModelForCausalLM.register(LlavaConfig, LlavaLlamaForCausalLM)
