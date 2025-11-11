31c7286fb4ad7d7c4c7c8e9361aa8ae69d07850a
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-11 15:52:17 +0900

    modify O-LoRA training setup.

--- LLaVA/OLoRA/scripts/MCITlib/Train/train_UCIT.sh
+++ LLaVA/OLoRA/scripts/MCITlib/Train/train_UCIT.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
+pip install -e .
+
 HARD_PATH=/home/data/vgilab/jeongeun/MCITlib
 
 bash scripts/MCITlib/Train/Task1.sh \
-    /home/data/vgilab/jeongeun/MCITlib/configs/modal_configs/llava.json \
-    /home/data/vgilab/jeongeun/MCITlib/configs/data_configs/UCIT/ImageNet-R.json \
-    /home/data/vgilab/jeongeun/MCITlib/configs/train_configs/OLoRA/LLaVA/UCIT/train/task1.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 1
+    $HARD_PATH/configs/modal_configs/llava.json \
+    $HARD_PATH/configs/data_configs/UCIT/ImageNet-R.json \
+    $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task1.json
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 1
 
-# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/ArxivQA.json \
     $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task2.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 2
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 2
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/VizWiz.json \
     $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task3.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 3
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 3
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/IconQA.json \
     $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task4.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 4
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 4
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/CLEVR-Math.json \
     $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task5.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 5
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 5
 
-pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/Flickr30k.json \
     $HARD_PATH/configs/train_configs/OLoRA/LLaVA/UCIT/train/task6.json
-bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 6
+# bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 6
