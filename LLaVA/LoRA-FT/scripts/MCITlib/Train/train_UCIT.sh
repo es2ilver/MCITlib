eb2821efc210d7ea50accd7e8e44ff6a07e7772a
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-28 17:00:02 +0900

    change ckpt paths, hard-coded gpu nums

--- LLaVA/LoRA-FT/scripts/MCITlib/Train/train_UCIT.sh
+++ LLaVA/LoRA-FT/scripts/MCITlib/Train/train_UCIT.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
-HARD_PATH=/mnt/haiyangguo/mywork/CL-MLLM/MCITlib_v2
+# GPU 4-7번 사용 설정
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-pip install -e .
+HARD_PATH=/home/data/vgilab/jeongeun/MCITlib
+
+# pip install -e .
 bash scripts/MCITlib/Train/Task1.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/ImageNet-R.json \
     $HARD_PATH/configs/train_configs/LoRA-FT/LLaVA/UCIT/train/task1.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 1
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/ArxivQA.json \
     $HARD_PATH/configs/train_configs/LoRA-FT/LLaVA/UCIT/train/task2.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 2
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/VizWiz.json \
     $HARD_PATH/configs/train_configs/LoRA-FT/LLaVA/UCIT/train/task3.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 3
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/IconQA.json \
     $HARD_PATH/configs/train_configs/LoRA-FT/LLaVA/UCIT/train/task4.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 4
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/CLEVR-Math.json \
     $HARD_PATH/configs/train_configs/LoRA-FT/LLaVA/UCIT/train/task5.json
 bash scripts/MCITlib/Eval_UCIT/Eval_finetune1.sh 5
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Taskn.sh \
     $HARD_PATH/configs/modal_configs/llava.json \
     $HARD_PATH/configs/data_configs/UCIT/Flickr30k.json \
