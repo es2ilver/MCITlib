1c46f65d0c2876d69f0f04d02ad7de193eba23c2
Jeongeun Lee <esilver@esilverui-MacBookAir.local>
2025-11-28 16:55:39 +0900

    change code for train LoRA-FT for UCIT benchmark.

--- InternVL/LoRA-FT/scripts/MCITlib/Train/train_UCIT.sh
+++ InternVL/LoRA-FT/scripts/MCITlib/Train/train_UCIT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HARD_PATH=/mnt/haiyangguo/mywork/CL-MLLM/MCITlib_v2
+HARD_PATH=/data/vgilab/jeongeun/MCITlib
 
-pip install -e .
+# pip install -e .
 bash scripts/MCITlib/Train/Task1.sh \
     $HARD_PATH/configs/modal_configs/internvl.json \
     $HARD_PATH/configs/data_configs/UCIT/ImageNet-R.json \
